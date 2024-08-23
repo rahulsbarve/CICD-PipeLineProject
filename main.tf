1ee62f033f120f1e942260592a23b24185515591
rahulsbarve <rahulsbarve@gmail.com>
2024-08-23 14:22:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "google_storage_bucket" "my-bucket" {
   name          = "storage-bucket-with-tf-and-git-actions"
   location      = "US"
   force_destroy = true
-
+  project = "opentelemetry-demo-428711"
   public_access_prevention = "enforced"
 }
